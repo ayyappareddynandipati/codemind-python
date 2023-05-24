a747b2b77232e406677ee3f589a7f5e0da2e7da7
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2023-05-24 23:09:56 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,n+1):         if (j==1 or j==n or i==j):             print("*",end=' ')         else:             print(" ",end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 n=int(input())
 for i in range(1,n+1):
     for j in range(1,n+1):
-        print("%c"%(i+64),end=' ')
+        if (j==1 or j==n or i==j):
+            print("*",end=' ')
+        else:
+            print(" ",end=' ')
     print()
